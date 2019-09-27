f8cc04af2578ea240aae34ff9659fa1a46ede67f
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-09-27 13:33:15 +0100

    Bump version: 1.0.5 → 1.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = [
 ]
 setup(
     name='event_service_utils',
-    version='1.0.5',
+    version='1.1.0',
     description="Event service utils",
     long_description=readme,
     author="Felipe Arruda Pontes",
