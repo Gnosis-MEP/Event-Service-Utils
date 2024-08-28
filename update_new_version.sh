0cd803972ded43236e736070acc9d56df601a99c
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2024-08-28 12:41:11 +0200

    adding new instruction and dep for uploading on public pypi with twine

--- update_new_version.sh
+++ update_new_version.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
+# bumpversion major, minor, patch
 python setup.py sdist
 
 CUR_VERSION=$(cat setup.cfg | grep "current_version = .*" | awk '{split($0,a,"\= "); print a[2]}')
 
-echo "new version: ${CUR_VERSION}"
-echo "copying to ${HEROKU_PYPI_LOCAL_DIR}/data/"
-cp dist/event_service_utils-${CUR_VERSION}.tar.gz ${HEROKU_PYPI_LOCAL_DIR}/data/
-pushd $HEROKU_PYPI_LOCAL_DIR
-echo "commiting changes in pypi repo"
-git add data/
-git commit -m 'new version'
-echo "pushing changes to heroku"
-git push origin master
-git push heroku master
-popd
+twine upload dist/event_service_utils-${CUR_VERSION}.tar.gz
